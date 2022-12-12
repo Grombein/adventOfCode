0fd1dbf361e70bd1bbcf2e532fbf2d99f0446174
Fredrik Grombein <grombein@gmail.com>
2022-12-12 14:13:49 +0100

    Advent Of Code Day 1 Final

--- 01.py
+++ 01.py
@@ -1,4 +1,21 @@
 with open('input.txt') as f:
-    lines = f.readlines()
+    lines = f.read().strip().split("\n")
 
-print(input.txt)
+
+calories = 0
+totalCals = []
+
+#Looping trough every "calorie" and resetting the count whenever an empty space in the document occurs
+for i in lines:
+    if i == '':
+        totalCals.append(calories)
+        calories = 0
+        
+#Changing every count in the input document from string to int so that we can summarize them
+    else:
+        num = int(i)
+        calories += num
+
+#Sorting the list and summarize the three highest values 
+totalCals.sort()
+print(totalCals[-1]+totalCals[-2]+totalCals[-3])
