72e79c4610cae055ce962b7518a3d6399e29328b
Fredrik Grombein <grombein@gmail.com>
2022-12-13 14:41:12 +0100

    Day 4 Final

--- 04/04.py
+++ 04/04.py
@@ -2,6 +2,7 @@ with open('input.txt') as f:
     data = [i for i in f.read().strip().split("\n")]
 
 
+#Part 1
 
 pairs = 0
 for i in data:
@@ -16,4 +17,18 @@ for i in data:
     elif second[0] <= first[0] and second[1] >= first[1]:
         pairs +=1
 
+# Part 2
+pairs = 0
+for i in data:
+    first, second = i.split(",")
+
+    first = [int(i) for i in first.split("-")]
+    second = [int(i) for i in second.split("-")]
+
+    if first[0] is range(second[0], second[1]+1) or first[1] in range(second[0], second[1]+1):
+        pairs += 1
+    elif second[0] is range(first[0], first[1]+1) or second[1] in range(first[0], first[1]+1):
+        pairs += 1
+
 print(pairs)
+
