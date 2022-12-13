5bb4c4b2ebc519c2e2c2cce6dd011870f827adb1
Fredrik Grombein <grombein@gmail.com>
2022-12-13 11:41:18 +0100

    Day 3 Final

---
+++ 03/03.py
@@ -0,0 +1,31 @@
+from string import ascii_letters
+
+#Loads in input-file and iterates through it
+with open('input.txt') as f:
+    data = [i for i in f.read().strip().split("\n")]
+
+#Looping trough the data and finding the unique character that exists in both sets
+totalSum = 0
+for rucksack in data:
+    half = len(rucksack)//2
+
+    left = set(rucksack[:half])
+    right = set(rucksack[half:])
+
+    for priority, char in enumerate(ascii_letters):
+        if char in left and char in right:
+            totalSum += priority + 1
+
+#Part 2
+
+jump = 3
+totalSum = 0
+for i in range(0, len(data), 3):
+    rucksacks = data[i:jump]
+    jump += 3
+
+    for priority, char in enumerate(ascii_letters):
+        if char in rucksacks[0] and char in rucksacks[1] and char in rucksacks[2]:
+            totalSum += priority +1
+
+print(totalSum)
